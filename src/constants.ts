66a1814b9ca8184377a1497f3becb5fb8e3b48fb
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-06-19 00:22:33 +0900

    Fixed package info reference

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
-// import { dirname } from 'path';
-// import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { readPackageUpSync } from 'read-pkg-up';
 
 /**
  * Package Info
  */
-// export const __dirname: string = dirname(fileURLToPath(import.meta.url));
-const manifest = readPackageUpSync();
-export const PACKAGE_JSON = manifest ? manifest.packageJson : undefined;
+export const __dirname: string = dirname(fileURLToPath(import.meta.url));
+export const PACKAGE_JSON =
+  readPackageUpSync({ cwd: __dirname })?.packageJson || undefined;
