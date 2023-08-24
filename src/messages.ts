4cc6788295c885dae30bc8df3b1e4fd9fd3d33af
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2023-08-24 17:31:27 +0900

    Add necessary error messages

--- src/messages.ts
+++ src/messages.ts
@@ -12,17 +12,16 @@ export const MESSAGES = {
       'The value of updateExistingGoogleSheets in the configuration file must be a boolean, i.e., it must be either true or false.',
     typeErrorSaveOriginalFilesToDriveMustBeBoolean:
       'The value of saveOriginalFilesToDrive in the configuration file must be a boolean, i.e., it must be either true or false.',
+    c2gErrorConfigFileMustContain4Properties:
+      'The configuration file must contain the following four properties: sourceDir, targetDriveFolderId, updateExistingGoogleSheets, and saveOriginalFilesToDrive.',
     c2gErrorConfigFileNotFound:
       'Enter a valid path for the configuration file. You can create a new one by running the command `c2g init`.',
     c2gErrorCredentialsFileNotFound: `${CREDENTIALS_FILE_NAME} not found.`,
     c2gErrorInvalidCredentials: `Invalid credentials. Please check the structure of your ${CREDENTIALS_FILE_NAME}.`,
+    c2gErrorNoCsvFilesFound: 'No CSV files found.',
     c2gErrorNotLoggedIn: 'You are not logged in. Please run `c2g login`.',
     c2gErrorSourceDirMustBeValidPath:
       'The value of sourceDir in the configuration file must be a valid path.',
-    c2gErrorTargetDriveFolderIdInvalid:
-      'The target Google Drive folder ID is either invalid or you do not have access to it. Please check the ID and update the config file.',
-    c2gErrorConfigFileMustContain4Properties:
-      'The configuration file must contain the following four properties: sourceDir, targetDriveFolderId, updateExistingGoogleSheets, and saveOriginalFilesToDrive.',
   },
   log: {
     loggingIn: 'Logging in...',
