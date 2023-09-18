b15a96be51159ac874fb2c21e0264a8afc233e99
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-09-19 03:40:39 +0900

    Move createConfigFile to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,74 @@
 
 import fs from 'fs';
 import { drive_v3 } from 'googleapis';
+import inquirer, { QuestionCollection } from 'inquirer';
 import path from 'path';
 
-import { Config, DEFAULT_CONFIG } from './constants';
+import { Config, CONFIG_FILE_NAME, DEFAULT_CONFIG } from './constants';
 import { C2gError } from './c2g-error';
 import { MESSAGES } from './messages';
+
+/**
+ * Creates a config file in the current directory based on user input
+ */
+export async function createConfigFile(): Promise<void> {
+  // Define the questions to be asked
+  const questions: QuestionCollection = [
+    {
+      name: 'sourceDir',
+      type: 'input',
+      message: MESSAGES.prompt.enterSourceDir,
+      default: DEFAULT_CONFIG.sourceDir,
+      validate: (value: string) => {
+        if (fs.existsSync(value)) {
+          return true;
+        } else {
+          return MESSAGES.prompt.enterValidPath;
+        }
+      },
+    },
+    {
+      name: 'targetDriveFolderId',
+      type: 'input',
+      message: MESSAGES.prompt.enterTargetDriveFolderId,
+      default: DEFAULT_CONFIG.targetDriveFolderId,
+      validate: (value: string) => {
+        if (value.length) {
+          return true;
+        } else {
+          return MESSAGES.prompt.enterValidId;
+        }
+      },
+    },
+    {
+      name: 'targetIsSharedDrive',
+      type: 'confirm',
+      message: MESSAGES.prompt.targetIsSharedDriveYN,
+      default: DEFAULT_CONFIG.targetIsSharedDrive,
+    },
+    {
+      name: 'updateExistingGoogleSheets',
+      type: 'confirm',
+      message: MESSAGES.prompt.updateExistingGoogleSheetsYN,
+      default: DEFAULT_CONFIG.updateExistingGoogleSheets,
+    },
+    {
+      name: 'saveOriginalFilesToDrive',
+      type: 'confirm',
+      message: MESSAGES.prompt.saveOriginalFilesToDriveYN,
+      default: DEFAULT_CONFIG.saveOriginalFilesToDrive,
+    },
+  ];
+
+  // Prompt the user for input
+  const answers = (await inquirer.prompt(questions)) as Config;
+
+  // Write the answers to a config file
+  fs.writeFileSync(
+    path.join(process.cwd(), CONFIG_FILE_NAME),
+    JSON.stringify(answers, null, 2),
+  );
+}
 
 /**
  * Read the configuration file and return its contents as an object.
