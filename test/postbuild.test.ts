f86beade81eb0194e7da7d6466e52440984fa7dc
ttsukagoshi <taro.tsukagoshi@gmail.com>
2023-09-28 06:41:49 +0900

    Modify test for `postbuild` to mock `fs` methods #101

--- test/postbuild.test.ts
+++ test/postbuild.test.ts
@@ -87,6 +87,7 @@ describe('createRegexpFromFileNames', () => {
 
 describe('replaceFileContent', () => {
   it('should replace the content of a file', () => {
+    // Arrange
     const fileContent =
       "import { test1 } from './test1';\nimport test2, { test2sub } from './test2';\nimport fs from 'fs';\nimport test2 from 'test2';\nimport * as utils from './test2';\nconst test2 = () => {\n  console.log('test2');\n};\n\nexport { test2 };\n";
     const regexp = new RegExp(
@@ -94,6 +95,7 @@ describe('replaceFileContent', () => {
       'gm',
     );
     const extension = '.js';
+    // Act & Assert
     expect(fileContent.match(regexp)).not.toBeNull();
     expect(postbuild.replaceFileContent(fileContent, regexp, extension)).toBe(
       "import { test1 } from './test1';\nimport test2, { test2sub } from './test2.js';\nimport fs from 'fs';\nimport test2 from 'test2';\nimport * as utils from './test2.js';\nconst test2 = () => {\n  console.log('test2');\n};\n\nexport { test2 };\n",
@@ -102,22 +104,51 @@ describe('replaceFileContent', () => {
 });
 
 describe('postbuild', () => {
-  const targetPath = path.join(process.cwd(), 'postbuild');
-  const testFilePath = path.join(targetPath, 'test.js');
-  const testFileContent = "import { authorize, isAuthorized } from '../test';";
-  const expectedFileContent =
-    "import { authorize, isAuthorized } from '../test.js';";
-
-  beforeEach(() => {
-    fs.writeFileSync(testFilePath, testFileContent, 'utf8');
-  });
-
   afterEach(() => {
-    fs.unlinkSync(testFilePath);
+    jest.restoreAllMocks();
   });
+
   it("should add '.js' to the part of the file content that matches the regular expressions created from the file names in the given directory and its subfolders", () => {
+    // Arrange
+    const targetPath = path.join(process.cwd());
+    const testFileContent =
+      "import { test1Func1, test1Func2 } from '../test1';";
+    jest
+      .spyOn(fs, 'readdirSync')
+      .mockReturnValue([
+        { name: 'test1.js', isDirectory: () => false } as unknown as fs.Dirent,
+        { name: 'test2.js', isDirectory: () => false } as unknown as fs.Dirent,
+        { name: 'test3.csv', isDirectory: () => false } as unknown as fs.Dirent,
+      ]);
+    jest.spyOn(fs, 'readFileSync').mockReturnValue(testFileContent);
+    jest.spyOn(fs, 'writeFileSync').mockImplementation();
+    // Act
     postbuild.postbuild(targetPath);
-    const modifiedFileContent = fs.readFileSync(testFilePath, 'utf8');
-    expect(modifiedFileContent).toEqual(expectedFileContent);
+    // Assert
+    expect(fs.readdirSync).toHaveBeenCalledTimes(1);
+    expect(fs.readFileSync).toHaveBeenCalledTimes(2);
+    expect(fs.readFileSync).toHaveBeenNthCalledWith(
+      1,
+      path.join(targetPath, 'test1.js'),
+      'utf8',
+    );
+    expect(fs.readFileSync).toHaveBeenNthCalledWith(
+      2,
+      path.join(targetPath, 'test2.js'),
+      'utf8',
+    );
+    expect(fs.writeFileSync).toHaveBeenCalledTimes(2);
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      1,
+      path.join(targetPath, 'test1.js'),
+      "import { test1Func1, test1Func2 } from '../test1.js';",
+      'utf8',
+    );
+    expect(fs.writeFileSync).toHaveBeenNthCalledWith(
+      2,
+      path.join(targetPath, 'test2.js'),
+      "import { test1Func1, test1Func2 } from '../test1.js';",
+      'utf8',
+    );
   });
 });
